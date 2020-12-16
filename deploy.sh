74cf89ce07578b4d073bf1196d126f8a17494056
Vlad <vlad.shaginyan@mail.ru>
2020-12-16 19:47:21 +0300

    configurate deploy files

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:7haggy/feedback-form.git master:gh-pages
+git push -f git@github.com:7haggy/client-form.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: "/feedback-form/",
+    publicPath: "/client-form/",
 };
